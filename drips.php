d1e99f0610a53995915d21e99f5025b906ed4e0f
Raffael Kessler <raffael.kessler@prowect.com>
2016-06-27 21:38:58 +0200

    generatePropelDNS

--- drips.php
+++ drips.php
@@ -5,22 +5,6 @@ use Drips\App;
 define('DRIPS_ORM_PROPEL_FILE', __DIR__.'/propel');
 define('DRIPS_ORM_PROPEL_CONFIG', __DIR__.'/drips.propel.php');
 
-function generatePropelDSN(){
-    $type = Config::get('database_type', 'sqlite');
-    $host = Config::get('database_host', DRIPS_SRC.'/database.sqlitedb');
-    $port = Config::get('database_port', 3306);
-    $db = Config::get('database_name', 'drips');
-
-    $dsn = $type.':';
-    if($type == 'sqlite'){
-        $dsn .= $host;
-    } else {
-        $dsn .= 'host=' . $host . ';port=' . $port . 'dbname=' . $db;
-    }
-
-    return $dsn;
-}
-
 if(class_exists('Drips\App')){
     $drips_propel = __DIR__.'/../../../propel';
     $drips_propel_php = __DIR__.'/../../../propel.php';
--- drips.propel.php
+++ drips.propel.php
@@ -3,6 +3,24 @@ require_once __DIR__.'/index.php';
 
 use Drips\Config\Config;
 
+if(!function_exists('generatePropelDSN')){
+	function generatePropelDSN(){
+	    $type = Config::get('database_type', 'sqlite');
+	    $host = Config::get('database_host', DRIPS_SRC.'/database.sqlitedb');
+	    $port = Config::get('database_port', 3306);
+	    $db = Config::get('database_name', 'drips');
+	
+	    $dsn = $type.':';
+	    if($type == 'sqlite'){
+	        $dsn .= $host;
+	    } else {
+	        $dsn .= 'host=' . $host . ';port=' . $port . 'dbname=' . $db;
+	    }
+	
+	    return $dsn;
+	}
+}
+
 return [
     'propel' => [
         'paths' => [
