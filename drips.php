7490875a36820e4bdbdd4d7f7aea791641bf71df
Raffael Kessler <raffael.kessler@prowect.com>
2016-06-27 21:30:05 +0200

    Bugfix copy propel files

--- drips.php
+++ drips.php
@@ -6,8 +6,8 @@ define('DRIPS_ORM_PROPEL_FILE', __DIR__.'/propel');
 define('DRIPS_ORM_PROPEL_CONFIG', __DIR__.'/drips.propel.php');
 
 if(class_exists('Drips\App')){
-    $drips_propel = '../../../propel';
-    $drips_propel_php = '../../../propel.php';
+    $drips_propel = __DIR__.'/../../../propel';
+    $drips_propel_php = __DIR__.'/../../../propel.php';
     if(DRIPS_DEBUG || !file_exists($drips_propel) || !file_exists($drips_propel_php)){
         copy(DRIPS_ORM_PROPEL_FILE, $drips_propel);
         copy(DRIPS_ORM_PROPEL_CONFIG, $drips_propel_php);
