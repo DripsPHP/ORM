86bf1a6389136ebecbb2052aa8bd1d73f95941a6
Raffael Kessler <raffael.kessler@prowect.com>
2016-06-28 21:39:27 +0200

    Config hinzugefügt

--- drips.php
+++ drips.php
@@ -1,6 +1,7 @@
 <?php
 
 use Drips\App;
+use Drips\ORM\Config;
 
 define('DRIPS_ORM_PROPEL_FILE', __DIR__.'/propel');
 define('DRIPS_ORM_PROPEL_CONFIG', __DIR__.'/drips.propel.php');
@@ -12,4 +13,6 @@ if(class_exists('Drips\App')){
         copy(DRIPS_ORM_PROPEL_FILE, $drips_propel);
         copy(DRIPS_ORM_PROPEL_CONFIG, $drips_propel_php);
     }
+
+    Config::create(@include($drips_propel_php));
 }
